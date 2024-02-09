03385950df799c7c96e5eeebd5c6bc3460f1b62f
IbrahimOladepo <ibrahim.oladepo@yahoo.com>
2024-02-09 11:44:29 -0600

    Added data and code for plotting the data

--- Visualize_Number_of_Active_Channels.py
+++ Visualize_Number_of_Active_Channels.py
@@ -23,7 +23,7 @@ Acknowledgments:
     ChatGPT was used in generating the code.
 
 License:
-    MIT License
+    **
 
 Example:
     Simply run the script to display the plot:
@@ -46,3 +46,30 @@ mpl.rcParams["axes.spines.top"] = False
 # Suppress annoying warnings!
 import logging
 logging.getLogger("matplotlib.font_manager").setLevel(logging.ERROR)
+
+# Data for Mouse 1423
+mouse_1423_channels = [52, 53, 48, 44, 25, 28, 8, 32]
+mouse_1423_days = [19, 26, 34, 40, 48, 54, 82, 92]
+
+# Data for Mouse 1479
+mouse_1479_channels = [59, 61, 60, 59, 59, 48, 37, 40]
+mouse_1479_days = [8, 16, 22, 30, 36, 58, 64, 74]
+
+# Data for Mouse 1481
+mouse_1481_channels = [62, 62, 62, 58]
+mouse_1481_days = [14, 21, 29, 35]
+
+# Plotting the data
+plt.figure(figsize=(5, 3), dpi=300)
+
+plt.plot(mouse_1423_days, mouse_1423_channels, marker='o', label='Mouse 1423')
+plt.plot(mouse_1479_days, mouse_1479_channels, marker='s', label='Mouse 1479')
+plt.plot(mouse_1481_days, mouse_1481_channels, marker='^', label='Mouse 1481')
+
+plt.xlabel('Number of Days Post Surgery')
+plt.ylabel('Number of Active Channels')
+plt.title('Active Channels Over Time Post-Surgery')
+plt.legend()
+# plt.grid(True)
+
+plt.show()
